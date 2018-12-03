5a3bbba51bf9a89eb1d780f84eed38cac00c7b98
mihaisaizu <mihaisorinsaizu@gmail.com>
2018-12-03 19:32:01 +0200

    Initial commit

---
+++ src/main/java/com/mihaisorin/springfw5webapp/Springfw5webappApplication.java
@@ -0,0 +1,12 @@
+package com.mihaisorin.springfw5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springfw5webappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springfw5webappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mihaisorin/springfw5webapp/Springfw5webappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mihaisorin.springfw5webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springfw5webappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
